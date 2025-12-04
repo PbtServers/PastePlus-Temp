09725973e7e9f195b5839a42a713014136beb2b3
PbtinaDev 3 <68289322+PbtServers@users.noreply.github.com>
2025-12-04 20:39:19 +0100

    Refactor installation commands in spicetify.sh
    Updated installation commands to download scripts first and execute them.

--- spicetify.sh
+++ spicetify.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Install CLI Base
-sudo curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sudo sh --root
+sudo curl -o "spice1.sh" https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sudo ./spice1.sh --root
+sudo rm spice1.sh
 
 # Install Marketplace resources
-sudo curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sudo sh --root
+sudo curl -o "spice2.sh" https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sudo ./spice2.sh --root
+sudo rm spice2.sh
 
 # Configure Spotify Prefs Flatpak
 sudo spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
