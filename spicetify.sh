16bccc783206d669c81f2f0889eb9003fe295f70
PbtinaDev 3 <68289322+PbtServers@users.noreply.github.com>
2025-12-04 20:55:10 +0100

    Reorder Marketplace resources installation

--- spicetify.sh
+++ spicetify.sh
@@ -9,16 +9,16 @@ sudo rm -rf ~/.config/spicetify
 curl -o "spice1.sh" https://raw.githubusercontent.com/spicetify/cli/main/install.sh && sh spice1.sh
 rm spice1.sh
 
-# Install Marketplace resources
-curl -o "spice2.sh" https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh && sh spice2.sh
-rm spice2.sh
-
 # Apply Permissions to Directories
 sudo chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
 sudo chmod a+wr -R /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps
 
 # Configure Spotify Prefs Flatpak
 spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
+
+# Install Marketplace resources
+curl -o "spice2.sh" https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh && sh spice2.sh
+rm spice2.sh
 
 # Update Spicetify
 spicetify update
