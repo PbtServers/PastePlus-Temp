b65a782bf37f24b118a9112c8605916b4940582f
PbtinaDev 3 <68289322+PbtServers@users.noreply.github.com>
2025-12-04 20:51:28 +0100

    Refactor Spicetify installation and update commands
    Removed sudo from Spicetify installation commands and updated restore process.

--- spicetify.sh
+++ spicetify.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Remove OLD Spicetify
+spicetify restore
+sudo rm -rf ~/.spicetify
+sudo rm -rf ~/.config/spicetify
+
 # Install CLI Base
-sudo curl -o "spice1.sh" https://raw.githubusercontent.com/spicetify/cli/main/install.sh && sudo sh spice1.sh --root
-sudo rm spice1.sh
+curl -o "spice1.sh" https://raw.githubusercontent.com/spicetify/cli/main/install.sh && sh spice1.sh
+rm spice1.sh
 
 # Install Marketplace resources
-sudo curl -o "spice2.sh" https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh && sudo sh spice2.sh
-sudo rm spice2.sh
-
-# Configure Spotify Prefs Flatpak
-sudo spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
+curl -o "spice2.sh" https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh && sh spice2.sh
+rm spice2.sh
 
 # Apply Permissions to Directories
 sudo chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
 sudo chmod a+wr -R /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps
 
+# Configure Spotify Prefs Flatpak
+spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
+
 # Update Spicetify
-sudo spicetify update
+spicetify update
