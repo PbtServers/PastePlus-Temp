611ae10c579a6c95648de16a8a9c86b63d3c9364
PbtinaDev 3 <68289322+PbtServers@users.noreply.github.com>
2025-12-04 21:43:08 +0100

    Add config fix and streamline installation process
    Added configuration fix and updated installation steps.

--- spicetify.sh
+++ spicetify.sh
@@ -5,6 +5,13 @@ spicetify restore
 sudo rm -rf ~/.spicetify
 sudo rm -rf ~/.config/spicetify
 
+# Create new Config Fix (CustomUser-Julyx)
+curl -o "config-xpui.ini" \
+https://raw.githubusercontent.com/PbtServers/PastePlus-Temp/refs/heads/master/config-xpui-CustomUser-Julyx.ini
+
+mkdir ~/.config/spicetify/
+mv config-xpui.ini ~/.config/spicetify/
+
 # Install CLI Base
 curl -o "spice1.sh" https://raw.githubusercontent.com/spicetify/cli/main/install.sh && sh spice1.sh
 rm spice1.sh
@@ -15,10 +22,9 @@ sudo chmod a+wr -R /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/
 
 # Configure Spotify Prefs Flatpak
 spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
+spicetify backup apply
+spicetify update
 
 # Install Marketplace resources
 curl -o "spice2.sh" https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh && sh spice2.sh
 rm spice2.sh
-
-# Update Spicetify
-spicetify update
