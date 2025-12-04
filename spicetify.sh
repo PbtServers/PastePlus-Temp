2226dcb859652e5bfe6b5b12cd25d473b3581695
PbtinaDev 3 <68289322+PbtServers@users.noreply.github.com>
2025-12-04 20:28:40 +0100

    Add sudo to spicetify.sh commands

--- spicetify.sh
+++ spicetify.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-sudo -S
-
 # Install CLI Base
-curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
+sudo curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sudo sh
 
 # Install Marketplace resources
-curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sh
+sudo curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sudo sh
 
 # Configure Spotify Prefs Flatpak
-spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
+sudo spicetify config prefs_path ~/.var/app/com.spotify.Client/config/spotify/prefs
 
 # Apply Permissions to Directories
 sudo chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
 sudo chmod a+wr -R /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps
 
 # Update Spicetify
-spicetify update
+sudo spicetify update
